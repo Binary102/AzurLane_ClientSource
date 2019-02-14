7985be85b1281615cfc1d890ad7d8f67df33751b
Binary102 <whyisiticantgetoutofbed@gmail.com>
2019-02-14 16:43:43 -0600

    CN Ver 1.9.129

--- Src/CN/view/login/loginmediator.lua
+++ Src/CN/view/login/loginmediator.lua
@@ -113,6 +113,8 @@ function slot0.handleNotification(slot0, slot1)
 				viewComponent = GatewayNoticeLayer
 			}))
 		end
+
+		getProxy(SettingsProxy):initEveryPlay()
 	elseif slot2 == GAME.USER_REGISTER_SUCCESS then
 		pg.MsgboxMgr.GetInstance():ShowMsgBox({
 			modal = true,
